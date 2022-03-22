79a31879a37ab1fdf42cdf0ddd576aed937aec48
Ashish <as3861425@gmail.com>
2022-03-22 18:04:13 +0530

    third commit

--- src/main/java/com/sterlite/test.java
+++ src/main/java/com/sterlite/test.java
@@ -4,6 +4,6 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to My Hell");
+		System.out.println("Welcome My world");
 		}
 }
